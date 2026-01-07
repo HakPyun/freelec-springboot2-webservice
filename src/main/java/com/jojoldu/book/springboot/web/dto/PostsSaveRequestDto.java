599151da0cc88d2fb333d41d0a2ebc8221d6a266
HakPyun <kjiz0503@gmail.com>
2026-01-07 22:10:56 +0900

    조회/수정 기능 코드 작성

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -5,8 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
+@Getter//getter와 field가 같이 있으면 jackson(JSON)이 get(field)의 property가 존재한다고 봄.
+@NoArgsConstructor//JSON을 dto에 담을 때 기본 생성자 필요. 역직렬화시 객체생성 해야해서 필요함(JSON -> Java Object)
 public class PostsSaveRequestDto {
     private String title;
     private String content;
@@ -25,4 +25,15 @@ public class PostsSaveRequestDto {
                 .author(author)
                 .build();
     }
+
+    //Jackson은 스프링부트에서 RequestBody, ResponseBody, RestController쓸 때 뒤에서 자동으로 쓰이는 것. 직렬화(객체 -> JSON) 역직렬화(JSON -> 객체) 담당
+
+    /* 리플렉션이 setter없이 값을 꽂아넣는 방법.
+    *
+    * Field field = Member.class.getDeclaredField("name");
+    * field.setAccessible(true);
+    * field.set(member, "kim");
+    *
+    */
+
 }
